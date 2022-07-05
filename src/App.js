043d1cc5de7e06322d3b199e6f7ebc6b0c8cd72f
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2022-07-05 16:02:24 +1200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import TodoList from './TodoList';
+
+function App() {
+  const [todos, setTodos] = useState([])
+
+  return (
+    <>
+    <TodoList todos={todos} />
+    <input type="text" />
+    <button>Add Todo</button>
+    <button>Clear Complete</button>
+    <div>0 left to do</div>
+    </>
+  )
+}
+
+export default App;
